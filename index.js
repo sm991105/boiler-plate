ce99d568e95d226735be5c4e3e37d1c7c7211f5b
Suemin Cho <sm991105@gmail.com>
2020-04-18 15:19:31 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,7 +25,19 @@ mongoose
 
 app.get("/", function (req, res) {
   // 익명함수
-  res.send("The start of the boiler-plate project.");
+  res.send("The start of the boiler-plate project!");
+});
+
+// Register page -> new router with post method
+app.post("/register", (req, res) => {
+  // Get information from client and save it in DB
+  const user = new User(req.body); // body-parser 로 인해 가능
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err }); // error 메시지와 함께 json 형식으로 전달
+    return res.status(200).json({
+      success: true,
+    });
+  }); // save 는 mongoDB method
 });
 
 app.listen(port, function () {
